6aca51307f900223495b8e92c6b3b615c68f28d7
Joosua Koskinen <joosua@seravo.com>
2021-06-15 14:15:12 +0300

    Example: site-info postbox

--- lib/postbox/template.php
+++ lib/postbox/template.php
@@ -14,10 +14,19 @@ if ( ! class_exists('Template') ) {
   class Template {
 
     /**
+     * Get simple paragraph component.
+     * @param string $paragraph Text to display.
+     * @return \Seravo\Postbox\Component Paragraph component.
+     */
+    public static function paragraph( $paragraph ) {
+      return Component::from_raw('<p>' . $paragraph . '</p>');
+    }
+
+    /**
      * Get paragraph component for showing error messages.
      * @param string $message Error message to display.
      * @return \Seravo\Postbox\Component Error component.
-     */
+     */                               
     public static function error_paragraph( $message ) {
       $html = sprintf('<p><b>%s</b></p>', $message);
       return Component::from_raw($html);
